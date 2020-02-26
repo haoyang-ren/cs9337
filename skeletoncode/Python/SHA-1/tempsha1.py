111947ac40d6df098c86d797b1a2808cec6220d0
Haoyang Ren <ryanreninau@gmail.com>
2020-02-27 00:01:19 +1100

    Complete test of SHA-1

--- skeletoncode/Python/SHA-1/tempsha1.py
+++ skeletoncode/Python/SHA-1/tempsha1.py
@@ -1,13 +1,34 @@
 import hashlib
-
+import sys
+import time
 
 #initializing string
 print '='*100 
-str = "SHA1 Clear text"
+#str = "SHA1 Clear text"
   
-result = hashlib.sha1(str.encode()) 
+plain_text = ""
+try:
+    source = open(sys.argv[1])
+    for line in source:
+        plain_text += line
+    source.close()
+except:
+    print("Cannot open source file!")
+    sys.exit()
+
+beforeEncrypt = time.time()
+result = hashlib.sha1(plain_text.encode()) 
+afterEncrypt = time.time()
+eTime = afterEncrypt - beforeEncrypt
+print("The encryption time is " + str(eTime))
   
 # printing the equivalent hexadecimal value. 
-print("The hexadecimal equivalent of SHA1 digest is : ") 
+print("The hexadecimal equivalent of SHA1 digest is : ")
+beforeDecrypt = time.time() 
 print(result.hexdigest())
+afterDecrypt = time.time()
+dTime = afterDecrypt - beforeDecrypt
+print("The decryption time is " + str(dTime))
+
+print("The total time is " + str(eTime + dTime))
 print '='*100 
