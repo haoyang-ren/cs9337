4ea79b64b2a4581ca2185151034f5ae4454242ad
Haoyang Ren <ryanreninau@gmail.com>
2020-02-28 13:07:26 +1100

    Compelete the HMAC generator

--- skeletoncode/Python/HMAC/tempHMAC.py
+++ skeletoncode/Python/HMAC/tempHMAC.py
@@ -1,7 +1,23 @@
 #Following code reads its source file and computes an HMAC signature for it:
 import hmac
-secret_key = 'secret-shared-key-goes-here'
+import sys
+import time
+#secret_key = 'secret-shared-key-goes-here'
+secret_key = ""
+try:
+    source = open(sys.argv[1])
+    for line in source:
+        secret_key += line
+    source.close()
+except:
+    print("Cannot open source file!")
+    sys.exit()
+
+beforeEncrypt = time.time()
 digest_maker = hmac.new(secret_key)#in your code replace key
+afterEncrypt = time.time()
+eTime = afterEncrypt - beforeEncrypt
+print("The encryption time is " + str(eTime))
 f = open('lorem.txt', 'rb')
 try:
     while True:
@@ -12,7 +28,13 @@ try:
 finally:
     f.close()
 
+beforeDecrypt = time.time() 
 digest = digest_maker.hexdigest()
+afterDecrypt = time.time()
+dTime = afterDecrypt - beforeDecrypt
+print("The decryption time is " + str(dTime))
+
+print("The total time is " + str(eTime + dTime))
 print '='*100 
 print "HMAC digest generated for \"lorem.txt\" file is:", digest
 print '='*100 
